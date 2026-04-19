8f5c86ad257155dc853b57b21053a4b19555cdb1
cesar160 <ctoledovillarreal@gmail.com>
2026-04-19 17:01:17 -0600

    fix: corregir archivos de configuracion faltantes

--- web_django/config/urls.py
+++ web_django/config/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.urls),
+    path('admin/', admin.site.urls),
     path('', include('tanque_app.urls')),
 ]
 
